948765e3dec7752fdbee66af9803a0ddd2d5d359
Sergey Pashinin <sergey@pashinin.com>
2013-08-15 22:27:14 +0400

    start emacs-server on Windows

--- elisp/init-server.el
+++ elisp/init-server.el
@@ -33,15 +33,14 @@
 ;; error: The directory `~/.emacs.d/server' is unsafe
 ;; 1. Create dir "~/.emacs.d/server"
 ;; 2. change owner to yourself
-;;(let ((start t))
-;;  (when (eq system-type 'windows-nt)
-;;    (let ((d (file-truename "~/.emacs.d/server")))
-;;      (unless (file-directory-p d) (make-directory d))   ; create "~/.emacs.d/server" dir
-;;      (setq start (= (shell-command (concat "icacls " d " /setowner " (user-login-name) " /T /C"))
-;;                     0))
-;;      ))
-;;  (if start (unless (server-running-p) (server-start))))
-;;(unless (server-running-p) (server-start))
+(let ((start t))
+  (when (eq system-type 'windows-nt)
+    (let ((d (file-truename "~/.emacs.d/server")))
+      (unless (file-directory-p d) (make-directory d))   ; create "~/.emacs.d/server" dir
+      (setq start (= (shell-command (concat "icacls " d " /setowner " (user-login-name) " /T /C"))
+                     0))
+      ))
+  (if start (unless (server-running-p) (server-start))))
 
 (provide 'init-server)
 ;;; init-server.el ends here
