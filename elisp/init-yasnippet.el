efdde8328ac7f8d1e4fe5d9d44b44946d3f87491
Sergey Pashinin <sergey@pashinin.com>
2013-08-20 19:40:28 +0400

    moved snippets to emacs.d/snippets/

--- elisp/init-yasnippet.el
+++ elisp/init-yasnippet.el
@@ -7,18 +7,14 @@
 
 (require 'yasnippet)
 
-(defun load-my-snippets ()
-  "Load my snippets."
-  (interactive)
-  (with-temp-message ""
-    (yas-load-directory "~/.emacs.d/snippets/django")
-    (yas-load-directory "~/.emacs.d/snippets/other")))
-;; (load-my-snippets)
-
 (setq yas-prompt-functions '(yas/dropdown-prompt yas/ido-prompt yas/x-prompt))
 (setq yas-wrap-around-region 'cua)
-(run-with-timer 1 nil 'load-my-snippets)
 (yas-global-mode 1)
+
+;;(setq yas/root-directory '("~/emacs.d/mysnippets"
+;;                           "~/Downloads/interesting-snippets"))
+;;;; Map `yas/load-directory' to every element
+;;(mapc 'yas/load-directory yas/root-directory)
 
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
