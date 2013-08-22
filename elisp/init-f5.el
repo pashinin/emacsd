72f80316c40ebed53fe41623e8c3586a47a47a92
Sergey Pashinin <sergey@pashinin.com>
2013-08-22 13:41:28 +0400

    "after compilation" function, .res file to g++

--- elisp/init-f5.el
+++ elisp/init-f5.el
@@ -11,27 +11,41 @@
   (python-shell-send-buffer t)
   )
 
+(defun after-compilation (status code msg)
+  "Run this function after compile."
+  (interactive)
+  ;; If M-x compile exists with a 0
+  ;; compile-command
+  ;;(when (and (eq status 'exit) (zerop code))
+  ;;  (if (executable-find "upx")
+  ;;      (shell-command (concat "upx -q C:/bin/hello/fileexists.exe"))))
+  ;; Always return the anticipated result of compilation-exit-message-function
+  (cons msg code))
+
 (defun my-magic-cpp (filename)
   "Do some magic with C/C++ file FILENAME."
   (interactive)
   (let ((f (file-truename filename))
         (ext (file-name-extension filename))
         (extl (downcase (file-name-extension filename)))
-        base gcc)
+        base gcc files resfile)
     (setq base (file-name-base f))
     (when (eq system-type 'windows-nt)
       (setq gcc "g++ -mwindows ")
       ;;(shell-command (concat gcc f " -o " base ".exe"))
       ;;(compile (concat gcc filename " -o " base ".exe -DCURL_STATICLIB"))
-      ;; TODO:
+
       ;; if have .res file (resources) - add it to the end
-      (compile (concat gcc filename " -o " base ".exe my.res"
+      (setq files (directory-files "." nil "\\.res$"))
+      (if (> (length files) 0) (setq resfile (car files)))
+
+      (setq compilation-exit-message-function 'after-compilation)
+      (compile (concat gcc filename " -o " base ".exe " (if resfile resfile)
                        ;;" -lcurl -lwsock32 -lidn -lwldap32"
                        ;;" -lssh2 -lrtmp -lcrypto -lz -lws2_32 -lwinmm -lssl"
                        ;;" -lboost_filesystem -lboost_system"
                        ;;" -luser32"
                        ))
-      ;; update dired buffer
       )))
 
 (defcustom var-libreoffice-exe nil
