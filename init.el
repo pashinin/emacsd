11639795eb50380771225572c005052dc4cd1e85
Sergey Pashinin <sergey@pashinin.com>
2013-10-03 03:33:59 +0400

    replaced .emacs with init.el

--- init.el
+++ init.el
@@ -126,6 +126,7 @@
   ;;(require 'auto-complete-yasnippet)
   (require 'init-capture nil 'noerror)
   (require 'init-f5)
+  (require 'init-scala)
   (require 'init-workgroups nil 'noerror)
   (require 'init-bookmarks)
   )
