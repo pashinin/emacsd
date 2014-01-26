a2b3993d38194eb9a175a154b4a599636356dbce
Sergey Pashinin <sergey@pashinin.com>
2014-01-27 01:32:20 +0400

    when instead of if, new helm-swoop

--- elisp/init-windows-buffers.el
+++ elisp/init-windows-buffers.el
@@ -15,33 +15,55 @@
 
 ;; Helm - choose buffer like a man
 ;; To configure colors: M-x customize-group <RET> helm <RET>
-;;(require 'helm)
-(if (require 'helm nil 'noerror)
-    (progn
-      (setq helm-idle-delay 0.1)
-      (setq helm-input-idle-delay 0.1)
-      (setq helm-c-locate-command "locate-with-mdfind %.0s %s")
-      ;; do not list:
-      ;;(loop for ext in '("\\.swf$" "\\.elc$" "\\.pyc$")
-      ;;      do (add-to-list 'helm-c-boring-file-regexp-list ext))
-      ;;(define-key global-map [(alt t)] 'helm-for-files)
-      ;;(global-set-key (kbd "<C-x C-b>") 'helm-for-files)
-      (global-set-key (kbd "C-x C-b") 'helm-mini)   ; usual Helm
-      (global-set-key (kbd "<S-s-insert>") 'helm-show-kill-ring)
-      ;; TODO: helm for GIT
-      ;; colors:
-      ;;(setq helm-candidate-number '((t (:background "deep sky blue" :foreground "black"))))
-      ;;(set-face helm-selection        '(t (:foreground "black" :weight bold)))
+(when (require 'helm nil 'noerror)
+  (setq helm-idle-delay 0.1
+        helm-input-idle-delay 0.1
+        helm-c-locate-command "locate-with-mdfind %.0s %s")
+  ;; do not list:
+  ;;(loop for ext in '("\\.swf$" "\\.elc$" "\\.pyc$")
+  ;;      do (add-to-list 'helm-c-boring-file-regexp-list ext))
+  ;;(define-key global-map [(alt t)] 'helm-for-files)
+  ;;(global-set-key (kbd "<C-x C-b>") 'helm-for-files)
+  (global-set-key (kbd "C-x C-b") 'helm-mini)   ; usual Helm
+  (global-set-key (kbd "<S-s-insert>") 'helm-show-kill-ring)
+  ;; TODO: helm for GIT
+  ;; colors:
+  ;;(setq helm-candidate-number '((t (:background "deep sky blue" :foreground "black"))))
+  ;;(set-face helm-selection        '(t (:foreground "black" :weight bold)))
 
-      ;; To open Helm in any other way (in the same buffer) - edit
-      ;; "helm-display-function" variable:
-      ;; Open in the same buffer:
-      (setq helm-display-function
-            (lambda (buf)
-              ;;(split-window-horizontally)
-              ;;(other-window 1)
-              (switch-to-buffer buf)))
-  ))
+  ;; To open Helm in any other way (in the same buffer) - edit
+  ;; "helm-display-function" variable:
+  ;; Open in the same buffer:
+  (setq helm-display-function
+        (lambda (buf)
+          ;;(split-window-horizontally)
+          ;;(other-window 1)
+          (switch-to-buffer buf)))
+
+
+  ;; https://github.com/ShingoFukuyama/helm-swoop
+  (require 'helm-swoop)
+  (global-set-key (kbd "M-i") 'helm-swoop)
+  (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+  (global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+  (global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+
+  ;; When doing isearch, hand the word over to helm-swoop
+  (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+  (define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+
+  ;; Save buffer when helm-multi-swoop-edit complete
+  (setq helm-multi-swoop-edit-save t)
+
+  ;; If this value is t, split window inside the current window
+  (setq helm-swoop-split-with-multiple-windows nil)
+
+  ;; Split direction. 'split-window-vertically or 'split-window-horizontally
+  (setq helm-swoop-split-direction 'split-window-vertically)
+
+  ;; If nil, you can slightly boost invoke speed in exchange for text color
+  (setq helm-swoop-speed-or-color nil)
+  )
 
 ;; Resize buffers
 (when (require 'windsize nil 'noerror)
