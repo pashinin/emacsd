9b954d2bece5a4dba8ee574ad80fff5070d9bbfc
Sergey Pashinin <sergey@pashinin.com>
2014-01-27 02:25:50 +0400

    Enable flycheck

--- elisp/init-flycheck.el
+++ elisp/init-flycheck.el
@@ -12,7 +12,24 @@
       flycheck-emacs-lisp-load-path load-path)
 
 ;;(add-hook 'after-init-hook #'global-flycheck-mode)
-;;(global-flycheck-mode)
+(global-flycheck-mode)
+
+
+(require 'flymake-python-pyflakes)
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+;;flycheck-flake8rc is a variable defined in `flycheck.el'.
+;;Its value is ".flake8rc"
+
+(require 'flymake-shell)
+(add-hook 'sh-set-shell-hook 'flymake-shell-load)
+
+(require 'flymake-yaml)
+(add-hook 'yaml-mode-hook 'flymake-yaml-load)
+
+(global-set-key (kbd "s-<") 'flymake-goto-prev-error)
+(global-set-key (kbd "s->") 'flymake-goto-next-error)
+
+;;(set-face-background 'flymake-warnline "dark slate blue")
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
