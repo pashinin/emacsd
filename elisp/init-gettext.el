b760ed70fd884a739cdfb94f16ea4beed7cb8a4e
Sergey Pashinin <sergey@pashinin.com>
2014-02-11 13:39:22 +0400

    po-mode: commented some lines, added keys help

--- elisp/init-gettext.el
+++ elisp/init-gettext.el
@@ -1,4 +1,4 @@
-;;; init-  --- description
+;;; init-gettext --- po-mode for editing translations files (.po)
 ;;; Commentary:
 ;;; Code:
 
@@ -8,14 +8,22 @@
 ;; apt-get isntall gettext-el
 ;; /usr/share/emacs24/site-lisp/gettext/po-mode.elc
 
+;; h - Help
+;; n - Find next
+;; p - Find prev
+;; u - Find next untranslated
+;; U - Find prev untranslated
+;; k - Turn the current entry into an untranslated
+;; Q - Quit processing and save the PO file
+
 (require 'po-mode)
-(autoload 'po-mode "po-mode"
-  "Major mode for translators to edit PO files" t)
+;;(autoload 'po-mode "po-mode"
+;;  "Major mode for translators to edit PO files" t)
 (setq auto-mode-alist (cons '("\\.po\\'\\|\\.po\\." . po-mode)
                             auto-mode-alist))
-(autoload 'po-find-file-coding-system "po-compat")
-(modify-coding-system-alist 'file "\\.po\\'\\|\\.po\\."
-                            'po-find-file-coding-system)
+;;(autoload 'po-find-file-coding-system "po-compat")
+;;(modify-coding-system-alist 'file "\\.po\\'\\|\\.po\\."
+;;                            'po-find-file-coding-system)
 
 (provide 'init-gettext)
 ;;; init-gettext.el ends here
