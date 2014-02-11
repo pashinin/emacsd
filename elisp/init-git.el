30769325d390d7254f73c2ce5ba625ab575d116c
Sergey Pashinin <sergey@pashinin.com>
2014-02-11 14:11:03 +0400

    Magit changes
    - key <C-s-g>
    - github-browse-file added
    - removed comments

--- elisp/init-git.el
+++ elisp/init-git.el
@@ -1,35 +1,24 @@
 ;;; init-git --- Git config
 ;;; Commentary:
+;;
+;; l l - short log
+;; E - Rebase
+;; M-n, M-p - reorder commits
+;;
+;; Windows (cygwin) has a bug (when using Magit "stash apply stash@{0}"):
+;; it destroys brackets "{", "}" - so need to escape them
+;; https://github.com/magit/magit/issues/522
+;;
 ;;; Code:
 
-;;(autoload 'magit-status "magit" nil t)
 (require 'magit)
-(define-key global-map "\C-xg" 'magit-status)
+(define-key global-map (kbd "C-s-g") 'magit-status)
 
-(setq ediff-prefer-iconified-control-frame nil)  ; these 2 are for magit merge help
-(setq ediff-prefer-long-help-message t)          ; window not to shrink to 1 column
-
-
-(setq ediff-split-window-function (if (> (frame-width) 150)
-                                      'split-window-horizontally
-                                    'split-window-vertically))
 (setq magit-set-upstream-on-push t)
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 
-;; windows shell has a bug (when using Magit "stash apply stash@{0}"):
-;; it destroys brackets "{", "}" - so need to escape them
-;; https://github.com/magit/magit/issues/522
-;;(if (eq system-type 'windows-nt)
-;;    (defadvice magit-run-git (before magit-run-git-win-curly-braces (&rest args) activate)
-;;      "Escape {} on Windows"
-;;      (setcar (nthcdr 2 args)
-;;              (replace-regexp-in-string "{\\([0-9]+\\)}" "\\\\{\\1\\\\}" (elt args 2)))))
-
-;;(defun my-magit-rebase-mode ()
-;;  ""
-;;  (interactive)
-;;  (local-set-key (kbd "M-f") 'newline-and-indent)
-;;  (local-set-key (kbd "M-b") 'newline-and-indent))
+(require 'github-browse-file)
+(global-set-key (kbd "<s-home>") 'github-browse-file)
 
 (provide 'init-git)
 ;;; init-git.el ends here
