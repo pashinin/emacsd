c1c5bdc327cbd9484f33b1699dde399d9562c3c9
Sergey Pashinin <sergey@pashinin.com>
2014-03-05 19:53:42 +0400

    Comments and small fixes in init-workgroups

--- elisp/init-workgroups.el
+++ elisp/init-workgroups.el
@@ -5,7 +5,7 @@
 (require 'init-variables)
 (add-to-list 'load-path (concat my-emacs-ext-dir "workgroups2/src"))
 
-;;(desktop-save-mode 1)
+;; (desktop-save-mode 1)   ; may have issues with workgroups
 
 (when (require 'workgroups2 nil 'noerror)
 
@@ -15,17 +15,28 @@
 (setq
  wg-mode-line-decor-left-brace "["
  wg-mode-line-decor-right-brace "]"
- wg-mode-line-only-name t
- wg-display-nowg nil)
+ wg-mode-line-only-name t           ; show only current WG name
+ wg-display-nowg nil                ; if no workgroups - display nothing
+ wg-mode-line-use-faces t           ; colorize mode line
+ wg-use-faces t                     ; colorize messages
+ )
+
+;; Keyboard shortcuts - load, save, switch
+(global-set-key (kbd "<pause>")     'wg-reload-session)
+(global-set-key (kbd "C-S-<pause>") 'wg-save-session)
+(global-set-key (kbd "s-z")         'wg-switch-to-workgroup)
+(global-set-key (kbd "s-/")         'wg-switch-to-previous-workgroup)
+;;(global-set-key (kbd "<s-f1>") (lambda () (interactive) (my-switch-wg-to "mail" )))
+;;(global-set-key (kbd "<s-f2>") (lambda () (interactive) (my-switch-wg-to "music")))
+
 
 ;; Define my functions
 (defun my-switch-wg-to (workgroup)
   "Switch to WORKGROUP (name) or return to previous one."
   (interactive)
   (if (equal (wg-workgroup-name (wg-current-workgroup)) workgroup)
-      (progn (wg-switch-to-previous-workgroup))
-    (progn
-      (wg-switch-to-workgroup workgroup)))
+      (wg-switch-to-previous-workgroup)
+    (wg-switch-to-workgroup workgroup))
   (message (wg-workgroup-name (wg-current-workgroup))))
 
 (defun test-and-load-workgroups ()
@@ -40,19 +51,6 @@
 
 (defun load-workgroups-if-needed()
   (run-with-idle-timer 0.5 nil 'test-and-load-workgroups))
-
-;; Keyboard shortcuts - load, save, switch
-(global-set-key (kbd "<pause>")     'wg-reload-session)
-(global-set-key (kbd "C-S-<pause>") 'wg-save-session)
-(global-set-key (kbd "s-z")         'wg-switch-to-workgroup)
-(global-set-key (kbd "s-/")         'wg-switch-to-previous-workgroup)
-(global-set-key (kbd "<s-f1>") (lambda () (interactive) (my-switch-wg-to "mail" )))
-(global-set-key (kbd "<s-f2>") (lambda () (interactive) (my-switch-wg-to "music")))
-
-;;(run-with-idle-timer 0.1 nil 'frame-workgroups)
-;;(add-hook 'after-make-frame-functions 'load-workgroups-if-needed)
-
-(setq wg-display-nowg nil)  ; if no workgroups - display nothing
 
 (defun set-my-frame-title (frame)
   "Set FRAME title format."
