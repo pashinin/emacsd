b123e48dd1f116d5e42572747e4f85feece200ec
Sergey Pashinin <sergey@pashinin.com>
2014-04-27 03:52:54 +0400

    Keys for helm-ls-git-ls

--- elisp/init-git.el
+++ elisp/init-git.el
@@ -22,7 +22,8 @@
 
 
 (require 'helm-ls-git)
-(global-set-key (kbd "C-x C-g") 'helm-ls-git-ls)
+(global-set-key (kbd "C-c C-g") 'helm-ls-git-ls)
+(global-set-key (kbd "s-]") 'helm-ls-git-ls)
 ;;(global-set-key (kbd "C-x C-d") 'helm-browse-project)
 
 
