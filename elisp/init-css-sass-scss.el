091beeb0441bcecdeaa29022b0087a525fe89d30
Sergey Pashinin <sergey@pashinin.com>
2014-04-27 04:11:25 +0400

    helm-css-scss

--- elisp/init-css-sass-scss.el
+++ elisp/init-css-sass-scss.el
@@ -66,5 +66,26 @@
   (add-hook 'css-mode-hook    'myHtmlStyle))
 
 
+(require 'helm)
+(require 'helm-css-scss)
+
+;; Allow comment inserting depth at each end of a brace
+(setq helm-css-scss-insert-close-comment-depth 2)
+;; If this value is t, split window appears inside the current window
+(setq helm-css-scss-split-with-multiple-windows nil)
+;; Split direction. 'split-window-vertically or 'split-window-horizontally
+(setq helm-css-scss-split-direction 'split-window-vertically)
+
+;; Set local keybind map for css-mode / scss-mode
+(dolist ($hook '(css-mode-hook scss-mode-hook less-css-mode-hook))
+  (add-hook
+   $hook (lambda ()
+           (local-set-key (kbd "s-i") 'helm-css-scss)
+           (local-set-key (kbd "s-I") 'helm-css-scss-back-to-last-point))))
+
+(define-key isearch-mode-map (kbd "s-i") 'helm-css-scss-from-isearch)
+(define-key helm-css-scss-map (kbd "s-i") 'helm-css-scss-multi-from-helm-css-scss)
+
+
 (provide 'init-css-sass-scss)
 ;;; init-css-sass-scss.el ends here
