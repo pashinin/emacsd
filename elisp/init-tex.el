bd569167474bd6cbe0148c9653cecfdc44f58f2c
Sergey Pashinin <sergey@pashinin.com>
2015-10-25 21:37:19 +0300

    use lower case

--- elisp/init-tex.el
+++ elisp/init-tex.el
@@ -54,16 +54,15 @@
 (setq-default TeX-master t)
 
 
-(add-hook 'LaTeX-mode-hook 'visual-line-mode)
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(add-hook 'latex-mode-hook 'visual-line-mode)
+(add-hook 'latex-mode-hook 'LaTeX-math-mode)
+(add-hook 'latex-mode-hook 'turn-on-reftex)
 ;;(setq reftex-plug-into-AUCTeX t)
-
 
 (defun turn-on-outline-minor-mode ()
   (outline-minor-mode 1))
 
-(add-hook 'LaTeX-mode-hook 'turn-on-outline-minor-mode)
+(add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
 (add-hook 'latex-mode-hook 'turn-on-outline-minor-mode)
 ;;(setq outline-minor-mode-prefix "\C-c \C-o") ; Or something else
 
